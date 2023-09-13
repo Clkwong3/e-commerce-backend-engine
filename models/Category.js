aecc4b709ac5bd4ce60c06da682c807b4e6bed34
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-09-12 21:41:54 -0700

    Created .env file and define the columns for category.js

--- models/Category.js
+++ models/Category.js
@@ -1,20 +1,31 @@
-const { Model, DataTypes } = require('sequelize');
+// Import necessary modules
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection.js");
 
-const sequelize = require('../config/connection.js');
-
+// Define Category model by extending Sequelize Model
 class Category extends Model {}
-
+// Initialize Category model
 Category.init(
   {
-    // define columns
+    // integer column that serves as the primary key and auto-increments
+    id: {
+      type: DataTypes.INTEGER, // 'id' column stores integer values
+      primaryKey: true, // Uniquely identifies each row
+      autoIncrement: true, // 'id' should auto-increment with each new row added
+    },
+    // string column that cannot be null and stores the name of the category
+    category_name: {
+      type: DataTypes.STRING, // 'category_name' column stores string values
+      allowNull: false, // 'category_name' cannot be null, must always have a value
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'category',
+    modelName: "category",
   }
 );
-
+// Export the Category model
 module.exports = Category;
