0c060272e293a89654c8bba155ebc8039b72210b
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-09-13 00:29:04 -0700

    Defined the connections and associations between the models in the index.js

--- models/index.js
+++ models/index.js
@@ -1,16 +1,34 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+// Import models
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+// 'Product' belongsTo 'Category'
+Product.belongsTo(Category, {
+  foreignKey: `category_id`,
+  // The 'Product' model contains a key (category_id) that links each product to a specific category.
+});
 
-// Categories have many Products
+// 'Category' hasMany 'Product'
+Category.hasMany(Product, {
+  foreignKey: `category_id`,
+  // A single category can be associated with many products because of the 'category_id' foreign key in the 'Product' Model.
+});
 
-// Products belongToMany Tags (through ProductTag)
+// 'Product' belongToMany 'Tag' (through 'ProductTag')
+Product.belongsToMany(Tag, {
+  through: ProductTag, // Establish the connection between products and tags (many-to-many relationship)
+  foreignKey: `product_id`, // Link products to tags
+  as: `tags`, // An alias 'tags' is used to access this association as 'tags' when querying
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: `tag_id`,
+  as: `products`,
+});
 
 module.exports = {
   Product,
