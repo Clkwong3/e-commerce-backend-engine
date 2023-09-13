da47b091e130c3d4344c1c89ca63213e0becfb1f
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-09-12 23:30:22 -0700

    Defined the columns for Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,19 +1,35 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection.js');
+const sequelize = require("../config/connection.js");
 
 class Tag extends Model {}
 
 Tag.init(
   {
-    // define columns
+    // Each tag is has a unique identifier and is automatically incremented
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false, // Must have value
+      primaryKey: true, // Main identifier
+      autoIncrement: true, // Auto-increment for each new entry
+    },
+    // Name of the tag that cannot be empty
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false, // Must have a name
+      unique: true, // Checks if the 'tag_name' is unique, prevent duplicates
+      validate: {
+        len: [1, 50], // Limits the length to 1 - 50 characters
+        isAlphanumeric: true, // Only alphanumeric characters are allowed
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
